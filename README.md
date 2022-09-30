29e94d91e1c22530db97ca3d6a4cc5f1955a1307
vikas11-creator <82199478+vikas11-creator@users.noreply.github.com>
2022-09-30 13:06:55 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-filter
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-omra2z)
---
+++ src/app/app.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  name = 'Angular';
+
+  array: any[] = [
+    {
+      id: 1,
+      department: 'it',
+      manager: 'male',
+      city: 'mumbai',
+    },
+    {
+      id: 1,
+      department: 'it',
+      manager: 'male',
+      city: 'mumbai',
+    },
+    {
+      id: 1,
+      department: 'it',
+      manager: 'male',
+      city: 'mumbai',
+    },
+    {
+      id: 1,
+      department: 'it',
+      manager: 'male',
+      city: 'mumbai',
+    },
+    {
+      id: 1,
+      department: 'it',
+      manager: 'male',
+      city: 'mumbai',
+    },
+    {
+      id: 1,
+      department: 'it',
+      manager: 'male',
+      city: 'mumbai',
+    },
+    {
+      id: 1,
+      department: 'it',
+      manager: 'male',
+      city: 'mumbai',
+    },
+  ];
+}
